1cf20e6817530c89e0e01913b3d0564fbf744e7f
Sh0bob <shobobg@gmail.com>
2026-03-17 00:16:52 +1100

    patch peace wrapper

--- FX Client_files/game.js
+++ FX Client_files/game.js
@@ -1127,7 +1127,14 @@ function pE() {
 	}, this.pf = function() {
 		aD.kj ? b8.pG.pf(aD.eo) : b0.pL.pg()
 	}, this.hp = function() {
-		aD.kj ? b8.pG.hp(aD.eo) : b0.pL.pY(513)
+		// If host, always use the alive peace start path
+        if (window.__TTWC && window.__TTWC.isHost) {
+            b8.pG.hp(aD.eo);
+            return;
+        }
+
+        // Otherwise use normal game logic
+        aD.kj ? b8.pG.hp(aD.eo) : b0.pL.pY(513)
 	}, this.hX = function(ig, eq, jY) {
 		aD.kj ? b8.pG.hX(aD.eo, ig, eq, jY) : b0.pL.ph(ig, eq, jY)
 	}
